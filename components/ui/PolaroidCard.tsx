a20d238ae6936b46ab638cbd103cb09d3be02020
ouassilabenhammou <509114@student.fontys.nl>
2025-11-20 02:30:15 +0100

    chore(ui): classname toegevoegd aan polaroidcard component

--- components/ui/PolaroidCard.tsx
+++ components/ui/PolaroidCard.tsx
@@ -4,11 +4,14 @@ import React from "react";
 interface PolaroidCardProps {
   src: string;
   text: string;
+  className?: string;
 }
 
-const PolaroidCard = ({ src, text }: PolaroidCardProps) => {
+const PolaroidCard = ({ src, text, className }: PolaroidCardProps) => {
   return (
-    <div className="w-[325px] h-[365px] flex flex-col items-center gap-[15px] p-3 shrink-0 bg-cremewhite shadow-md ">
+    <div
+      className={`w-[325px] h-[365px] flex flex-col items-center gap-[15px] p-3 shrink-0 bg-cremewhite shadow-md ${className}`}
+    >
       <div className="relative w-full h-[280px] overflow-hidden">
         <Image src={src} alt={text} fill className="object-cover" />
       </div>
