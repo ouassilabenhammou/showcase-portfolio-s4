7e6dbd8e81a9941c7eb713eb4a8f83a5b20e5e69
ouassilabenhammou <509114@student.fontys.nl>
2025-11-21 00:53:10 +0100

    style(contact): eerste layout voor contact section toegevoegd

--- components/home/Contact.tsx
+++ components/home/Contact.tsx
@@ -1,5 +1,32 @@
-import React from "react";
+import Image from "next/image";
+import SchuifButton from "../ui/SchuifButton";
 
 export default function Contact() {
-  return <section id="contact">Contact</section>;
+  return (
+    <section id="contact" className="h-[500px] flex flex-col">
+      <h1 className="font-heading text-[48px] flex justify-center mb-[66px]">
+        CONTACT
+      </h1>
+      <div className="grid grid-cols-3">
+        <div className="border border-black flex items-center ">
+          mail
+          {/* <Image
+            src="/illustrations/mail.svg"
+            alt="Zon illustratie"
+            width={275}
+            height={0}
+          /> */}
+        </div>
+        <div className="flex flex-col items-center gap-[50px] ">
+          <p className="text-center">
+            Interesse in een samenwerking? <br /> Neem contact op
+          </p>
+          <SchuifButton
+            text="Neem contact"
+            href="mailto:ouassila_01@outlook.com"
+          />
+        </div>
+      </div>
+    </section>
+  );
 }
