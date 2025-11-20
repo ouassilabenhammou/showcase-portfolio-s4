d34800485ac22e0ed58e065f57502aa57cb103a5
ouassilabenhammou <509114@student.fontys.nl>
2025-11-20 01:34:42 +0100

    style(hero): eerste layout voor hero section toegevoegd

--- components/home/HeroSection.tsx
+++ components/home/HeroSection.tsx
@@ -1,9 +1,18 @@
-import React from "react";
+import PolaroidCard from "../ui/PolaroidCard";
 
 export default function HeroSection() {
   return (
-    <section id="#" className="min-h-screen">
-      Hero
+    <section id="#" className="min-h-screen grid grid-cols-3 items-end">
+      <div className="col-span-2 leading-none">
+        <h1 className="font-heading text-[175px]">HI</h1>
+        <h1 className="font-heading text-[175px]">IK BEN</h1>
+        <h1 className="font-heading text-[225px]">OUASSILA</h1>
+      </div>
+
+      {/* rotate-4 nog toevoegen*/}
+      <div className="justify-self-end mb-7 ">
+        {/* <PolaroidCard src="/images/ditbenik.png" text="Dit ben ik" /> */}
+      </div>
     </section>
   );
 }
