dc68bd37ea6e5115d4b0a92631c84ccf47bc6967
ouassilabenhammou <509114@student.fontys.nl>
2025-11-21 03:14:10 +0100

    style(footer): opmaak voor de footer toegevoegd

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -3,29 +3,36 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="flex gap-[25px] h-[200px]">
-      <div className="border border-black flex items-end overflow-hidden ">
-        <span className="font-heading text-[225px] leading-[0.9]">
+    <footer className="flex h-[200px] items-end justify-between">
+      <div className="flex items-end ">
+        {/* Naam */}
+        <span className="text-cremewhite/10 font-heading text-[225px] leading-[0.9]">
           OUASSILA
         </span>
       </div>
-      <div className="flex flex-col justify-between flex-1">
-        <div className="border border-black flex justify-between">
-          <div className="flex flex-col">
-            <a href="#over-mij">Over mij</a>
-            <a href="#projecten">Projecten</a>
-            <a href="#contact">Contact</a>
+
+      <div className="flex flex-col justify-between">
+        {/* Nav-links */}
+        <nav>
+          <div className="flex justify-between">
+            <div className="flex flex-col">
+              <a href="#over-mij">Over mij</a>
+              <a href="#projecten">Projecten</a>
+              <a href="#contact">Contact</a>
+            </div>
+            <div className="flex flex-col ">
+              <Link href="/">Mail</Link>
+              <Link href="/">LinkedIn</Link>
+            </div>
+            <div className="flex flex-col ">
+              <Link href="/">GitHub</Link>
+              <Link href="/">404</Link>
+            </div>
           </div>
-          <div className="flex flex-col">
-            <Link href="/">Mail</Link>
-            <Link href="/">LinkedIn</Link>
-          </div>
-          <div className="flex flex-col">
-            <Link href="/">GitHub</Link>
-            <Link href="/">404</Link>
-          </div>
-        </div>
-        <div className="border border-black overflow-hidden flex justify-between font-heading text-[125px] leading-none">
+        </nav>
+
+        {/* © 2025 */}
+        <div className="text-cremewhite/10 flex justify-between font-heading text-[125px] leading-none gap-[27px]">
           <span>©</span>
           <span>2025</span>
         </div>
