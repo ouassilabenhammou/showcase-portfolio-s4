6dbfe473b95e8e07e1d34a6a7d2a0f76e3984e95
ouassilabenhammou <509114@student.fontys.nl>
2025-11-21 02:16:05 +0100

    style(over-mij): opmaak voor over mij toegevoegd

--- components/home/OverMij.tsx
+++ components/home/OverMij.tsx
@@ -7,9 +7,9 @@ export default function OverMij() {
       className="min-h-screen flex flex-col items-center justify-center"
     >
       <h1 className="font-heading text-[48px]">OVER MIJ</h1>
-      <div className="h-150 grid grid-cols-3 ">
-        <div className="border border-black flex flex-col justify-between">
-          <div>
+      <div className="h-[600px] w-full grid grid-cols-3 ">
+        <div className="flex flex-col justify-between">
+          <div className="max-w-[300px]">
             <p>
               Ik ben Ouassila en ik studeer ICT aan Fontys Hogescholen en zit in
               mijn vierde semester, waar ik me specialiseer in front-end
@@ -19,18 +19,16 @@ export default function OverMij() {
             </p>
           </div>
           <div>
-            Zon
-            {/* <Image
+            <Image
               src="/illustrations/zon.svg"
               alt="Zon illustratie"
               width={150}
               height={150}
-            /> */}
+            />
           </div>
         </div>
-        <div className="border border-black flex items-center justify-center">
-          Polaroid cards
-          {/* <div className="relative w-[325px] h-[365px]">
+        <div className="flex items-center justify-center">
+          <div className="relative w-[325px] h-[365px]">
             <div className="absolute top-0 left-0 rotate-3">
               <PolaroidCard
                 src="/images/ditbenikweer.png"
@@ -39,27 +37,29 @@ export default function OverMij() {
             </div>
 
             <div className="absolute top-0 left-0 ">
-              <PolaroidCard src="/images/ditbenik.png" text="Dit ben ik weer" />
+              <PolaroidCard src="/images/ditbenik.png" text="Dit ben ik" />
             </div>
-          </div> */}
+          </div>
         </div>
-        <div className="border border-black flex flex-col justify-between">
+        <div className="flex flex-col justify-between">
           <div className="place-self-end">
-            Bloem
-            {/* <Image
+            <Image
               src="/illustrations/bloem.svg"
               alt="Bloem illustratie"
               width={130}
               height={130}
-            /> */}
+            />
           </div>
-          <div>
+          <div className=" max-w-[300px] space-y-3 place-self-end ">
             <p>
               Ik hou van projecten die me uitdagen om iets nieuws te leren. Als
               ik eenmaal in een idee duik, wil ik weten hoe het werkt en vooral
-              hoe ik het beter kan maken. Mijn favoriete project tot nu toe? Een
-              persoonlijk experiment waarin ik met verschillende AI-tools
-              werkte.
+              hoe ik het beter kan maken.
+            </p>
+            <p className="whitespace-pre-line">
+              Mijn favoriete project tot nu toe?
+              {"\n"}Een persoonlijk experiment waarin ik met verschillende
+              AI-tools werkte.
             </p>
           </div>
         </div>
