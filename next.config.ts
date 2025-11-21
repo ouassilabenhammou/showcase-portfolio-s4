1a51ecfbf1517981bbe6dd06942429bb4c6ce04d
ouassilabenhammou <509114@student.fontys.nl>
2025-11-21 15:03:59 +0100

    chore(config): plaatjes van placehold.co werken nu

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    domains: ["placehold.co"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+    ],
+    dangerouslyAllowSVG: true,
+  },
+
   turbopack: {
     rules: {
       "*.svg": {
