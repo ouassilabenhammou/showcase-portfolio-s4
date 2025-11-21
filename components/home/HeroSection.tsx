c6c6fe5c124991dca28a506d2430f30ab781b5b6
ouassilabenhammou <509114@student.fontys.nl>
2025-11-21 01:47:19 +0100

    style(hero): opmaak van de hero section toegevoegd

--- components/home/HeroSection.tsx
+++ components/home/HeroSection.tsx
@@ -1,4 +1,5 @@
 import PolaroidCard from "../ui/PolaroidCard";
+import Image from "next/image";
 
 export default function HeroSection() {
   return (
@@ -6,12 +7,24 @@ export default function HeroSection() {
       <div className="col-span-2 leading-none">
         <h1 className="font-heading text-[175px]">HI</h1>
         <h1 className="font-heading text-[175px]">IK BEN</h1>
-        <h1 className="font-heading text-[225px]">OUASSILA</h1>
+        <h1 className="font-heading text-[225px] relative whitespace-nowrap">
+          OUASS
+          <span className="relative inline-block align-top">
+            I
+            <Image
+              src="/illustrations/punt.svg"
+              alt="Punt"
+              width={51}
+              height={50}
+              className="absolute left-1/2 -translate-x-1/2 -top-[55px]"
+            />
+          </span>
+          LA
+        </h1>
       </div>
 
-      {/* rotate-4 nog toevoegen*/}
-      <div className="justify-self-end mb-7 ">
-        {/* <PolaroidCard src="/images/ditbenik.png" text="Dit ben ik" /> */}
+      <div className="justify-self-end mb-7 rotate-4">
+        <PolaroidCard src="/images/ditbenik.png" text="Dit ben ik" />
       </div>
     </section>
   );
