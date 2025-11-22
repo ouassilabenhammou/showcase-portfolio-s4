9ae1de90b69c2e62ba0471e7cb3aa131c399d823
ouassilabenhammou <509114@student.fontys.nl>
2025-11-22 21:26:42 +0100

    chore(config): supabase domein toegevoegd
    Supabase domein toegevoegd naast placehold.co, zodat Next.js afbeeldingen vanuit supabase storage kan laden.

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["placehold.co"],
+    domains: ["placehold.co", "cxiykhrjcilfgdfwcsjd.supabase.co"],
     remotePatterns: [
       {
         protocol: "https",
